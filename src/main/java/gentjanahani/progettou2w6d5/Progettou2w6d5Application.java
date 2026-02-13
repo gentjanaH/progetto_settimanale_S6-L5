23879f3e4427ae109d41bc13dd16c5f749b4fe74
Gentjana <hanigentjana@gmail.com>
2026-02-13 09:05:44 +0100

    add_progetto_settimanale

---
+++ src/main/java/gentjanahani/progettou2w6d5/Progettou2w6d5Application.java
@@ -0,0 +1,13 @@
+package gentjanahani.progettou2w6d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettou2w6d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettou2w6d5Application.class, args);
+	}
+
+}
---
+++ src/test/java/gentjanahani/progettou2w6d5/Progettou2w6d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package gentjanahani.progettou2w6d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettou2w6d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
